b0cd32171b8228e59deb3ad4dfabcc7f0a6f461f
Konstantinos Koutsilis <50744802+koutsilis1999@users.noreply.github.com>
2021-01-02 00:54:59 +0200

    fixed access controll

--- teacher/partials/login-check.php
+++ teacher/partials/login-check.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!isset($_SESSION["user"])) {
+if (!isset($_SESSION["username"])) {
     $_SESSION["no-login"] = "<div class='error text-center'>Please login</div>";
     header("location:" . SITEURL . "teacher/login.php");
 }
