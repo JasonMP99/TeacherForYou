714aa5bfb44ffaf7e341a8501b8615ab15188d7e
Konstantinos Koutsilis <50744802+koutsilis1999@users.noreply.github.com>
2021-01-02 00:31:09 +0200

    tehacers can now see the bookings

--- teacher/bookings.php
+++ teacher/bookings.php
@@ -15,8 +15,10 @@
                 <th>Booking id</th>
                 <th>Full name</th>
                 <th>Email</th>
-                <th>Username</th>
-                <th>Action</th>
+                <th>Phone number</th>
+                <th>Curse</th>
+                <th>Date</th>
+                <th>Hours</th>
             </tr>
             <?php
             $sql = "SELECT * FROM bookings WHERE teacher_id=$id";
@@ -26,33 +28,31 @@
                 $count = mysqli_num_rows($res);
 
                 if ($count > 0) {
-                    $rows = mysqli_fetch_assoc($res);
-                    //get data
-                    $id = $rows["id"];
-                    $firstname = $rows["fullname"];
-                    $lastname = $rows["lastname"];
-                    $username = $rows["username"];
+                    while ($rows = mysqli_fetch_assoc($res)) {
+                        //get data
+                        $id = $rows["id"];
+                        $full_name = $rows["full_name"];
+                        $email = $rows["email"];
+                        $phone = $rows["phone"];
+                        $curse = $rows["curse"];
+                        $date = $rows["date"];
+                        $hours = $rows["hours"];
 
-                    //display
-                    ?>
-                    <tr>
-                        <td><?php echo $id ?></td>
-                        <td><?php echo $firstname ?></td>
-                        <td><?php echo $lastname ?></td>
-                        <td><?php echo $username ?></td>
-                        <td>
-                            <a href="<?php echo SITEURL; ?>teacher/update-password.php?id=<?php echo $id; ?>"
-                               class="btn-primary">Change Password</a>
-                            <a href="<?php echo SITEURL; ?>teacher/update-teacher.php?id=<?php echo $id; ?>"
-                               class="btn-secondary">Update</a>
-                            <a href="<?php echo SITEURL; ?>teacher/delete-teacher.php?id=<?php echo $id; ?>"
-                               class="btn-danger">Delete</a>
-                        </td>
-                    </tr>
-                    <?php
+                        //display
+                        ?>
+                        <tr>
+                            <td><?php echo $id; ?></td>
+                            <td><?php echo $full_name; ?></td>
+                            <td><?php echo $email; ?></td>
+                            <td><?php echo $phone; ?></td>
+                            <td><?php echo $curse; ?></td>
+                            <td><?php echo $date; ?></td>
+                            <td><?php echo $hours; ?></td>
+                        </tr>
+                        <?php
+                    }
                 }
             }
-
 
             ?>
         </table>
