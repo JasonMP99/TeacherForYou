cea05a2388097a7180e20458dea984da23bfbb3e
Konstantinos Koutsilis <50744802+koutsilis1999@users.noreply.github.com>
2021-01-02 14:16:55 +0200

    added search functionality so that a user can serach courses or a teacher, changed search background image

--- search.php
+++ search.php
@@ -2,6 +2,7 @@
     <!--         search Section Starts Here -->
     <section class="search text-center">
         <div class="container">
+
             <form action="<?php echo SITEURL; ?>search.php" method="POST">
                 <input type="search" name="search" placeholder="Search for Teacher or Course.." required>
                 <input type="submit" name="submit" value="search" class="btn btn-primary">
@@ -9,53 +10,16 @@
 
         </div>
     </section>
-    <!--        search Section Ends Here -->
+    <!--         search Section ends Here -->
 
-    <!--     CAtegories Section Starts Here -->
-    <!--    <section class="categories">-->
-    <!--        <div class="container">-->
-    <!--            <h2 class="text-center">Explore Foods</h2>-->
-    <!---->
-    <!--            <a href="category-foods.html">-->
-    <!--                <div class="box-3 float-container">-->
-    <!--                    <img src="images/pizza.jpg" alt="Pizza" class="img-responsive img-curve">-->
-    <!---->
-    <!--                    <h3 class="float-text text-white">Pizza</h3>-->
-    <!--                </div>-->
-    <!--            </a>-->
-    <!---->
-    <!--            <a href="#">-->
-    <!--                <div class="box-3 float-container">-->
-    <!--                    <img src="images/burger.jpg" alt="Burger" class="img-responsive img-curve">-->
-    <!---->
-    <!--                    <h3 class="float-text text-white">Burger</h3>-->
-    <!--                </div>-->
-    <!--            </a>-->
-    <!---->
-    <!--            <a href="#">-->
-    <!--                <div class="box-3 float-container">-->
-    <!--                    <img src="images/momo.jpg" alt="Momo" class="img-responsive img-curve">-->
-    <!---->
-    <!--                    <h3 class="float-text text-white">Momo</h3>-->
-    <!--                </div>-->
-    <!--            </a>-->
-    <!---->
-    <!--            <div class="clearfix"></div>-->
-    <!--        </div>-->
-    <!--    </section>-->
-    <!-- Categories Section Ends Here -->
-
-    <!-- teacher Section Starts Here -->
+    <!--  Course Starts Here -->
     <section class="teacher">
         <div class="container">
             <h2 class="text-center">Teachers</h2>
             <?php
-            if (isset($_SESSION["booked"])) {
-                echo $_SESSION["booked"];
-                unset($_SESSION["booked"]); //removes message
-            } ?>
-            <?php
-            $sql = "SELECT * FROM teacher";
+            $search = $_POST["search"];
+
+            $sql = "SELECT * FROM teacher WHERE courses LIKE '%$search%' OR firstname LIKE '%$search%' OR lastname LIKE '%$search%'";
             $res = mysqli_query($conn, $sql);
 
             $sn = 1; // sn to display rather than id
@@ -100,6 +64,10 @@
                         </div>
                         <?php
                     }
+                } else {
+                    ?>
+                    <p class="text-center">No results found.</p>
+                    <?php
                 }
             }
             ?>
@@ -112,5 +80,6 @@
         <!--            <a href="#">See All Foods</a>-->
         <!--        </p>-->
     </section>
-    <!-- teacher Section Ends Here -->
+    <!--        course Section Ends Here -->
+
 <?php include("partials-front/footer.php"); ?>
