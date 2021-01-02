c2726e46fc4d4402c2201d50da6c11a7707f9f33
Konstantinos Koutsilis <50744802+koutsilis1999@users.noreply.github.com>
2021-01-02 15:20:46 +0200

    changed login and register title and menu

--- login.php
+++ login.php
@@ -1,11 +1,39 @@
-<?php include("partials-front/menu.php") ?>
-
-    <html>
+<?php include("config/constants.php"); ?>
+    <!DOCTYPE html>
+    <html lang="en">
     <head>
-        <title>Login -TeacherForYou</title>
-        <link rel="stylesheet" href="css/teacher.css">
+        <meta charset="UTF-8">
+        <!-- Important to make website responsive -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login - TeacherForYou</title>
+
+        <!-- Link our CSS file -->
+        <link rel="stylesheet" href="css/style.css">
     </head>
+
 <body>
+    <!-- Navbar Section Starts Here -->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="#" title="Logo">
+                    <img src="images/logo.jpg" alt="TFU logo" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Navbar Section Ends Here -->
+
     <div class="login">
         <h1 class="text-center">Login</h1>
         <br>
--- register.php
+++ register.php
@@ -1,11 +1,37 @@
-<?php include("partials-front/menu.php") ?>
-
-<html>
+<?php include("config/constants.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Register -TeacherForYou</title>
-    <link rel="stylesheet" href="css/teacher.css">
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register - TeacherForYou</title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
+<!-- Navbar Section Starts Here -->
+<section class="navbar">
+    <div class="container">
+        <div class="logo">
+            <a href="#" title="Logo">
+                <img src="images/logo.jpg" alt="TFU logo" class="img-responsive">
+            </a>
+        </div>
+
+        <div class="menu text-right">
+            <ul>
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="clearfix"></div>
+    </div>
+</section>
 <div class="register">
     <h1 class="text-center">Register</h1>
     <br/>
