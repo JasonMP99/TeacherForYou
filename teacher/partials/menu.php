97befee0b11a6cce90ab8f590610ecc69f59a4cb
JasonMp <jason29899@gmail.com>
2021-01-10 22:38:18 +0200

    make some pages more phone responsive

--- teacher/partials/menu.php
+++ teacher/partials/menu.php
@@ -5,6 +5,9 @@ include("login-check.php");
 
 <html>
 <head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TeacherForYou- Home page</title>
     <link rel="icon" href="../images/tfy-logo.png" type="image/icon type">
     <link rel="stylesheet" href="../css/teacher.css">
